c07e70be1f377d9c0ff608e8270da0b4e8bb2151
justecii <justecii@gmail.com>
2017-10-11 09:43:12 -0700

    like halfway done

--- es6-exercises/12-classes.js
+++ es6-exercises/12-classes.js
@@ -6,8 +6,18 @@
 // of the two original points, and whose y is the sum of their y properties.
 
 // Your code here
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+    }
+    plus(pt) {
+        return new Point(this.x + pt.x, this.y + pt.y)
+    }
+}
 console.log(new Point(1, 2).plus(new Point(2, 1)))
-// → Point{x: 3, y: 3}
+    // → Point{x: 3, y: 3}
 
 
 // source: http://marijnhaverbeke.nl/talks/es6_falsyvalues2015/exercises/#Point
