c6ed39b59dc83b5cff7d78ff08a5bcadd9ef29cc
laurenperez <laurenashleymarks@gmail.com>
2017-10-13 13:15:10 -0700

    finished as many as I could - will come back to this

--- es6-exercises/02-const-complex.js
+++ es6-exercises/02-const-complex.js
@@ -9,8 +9,18 @@ const account = {
   password: "xyzzy"
 }
 
+Object.freeze( account ); //now we cannot change the password
+
 account.password = "s3cret"
 
 console.log(account.password)
 
+
+
 // source: http://marijnhaverbeke.nl/talks/es6_falsyvalues2015/exercises/#Constant_non-constance
+
+
+// ******Solution - yes const will allow us to change the value for password, you can
+// change the values just not the object itself.
+// If we want to make it so that we cannot change the vaules in the object we use an object freeze.
+// this makes It makes an object immutable, i.e. you cannot change its properties.   *********
--- es6-exercises/04-default-parameters.js
+++ es6-exercises/04-default-parameters.js
@@ -2,8 +2,13 @@
 // and lastName. Set the default values to 'hello' and your first
 // and last name, if no input is provided.
 
-const greeting = function(){}  // fill this in!
+const greeting = function(greeting = "hello", firstName ="lauren", lastName ="perez") => {
+  return `${greeting} ${firstName} ${lastName}`;
+}  // DONE!
 
 console.log(greeting());
 
 console.log(greeting('hi', 'harry', 'hedger'));
+
+
+// Solution : function(greeting = "hello", firstName ="lauren", lastName ="perez")
